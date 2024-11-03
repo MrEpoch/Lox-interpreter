14dae341de2a3807ffbbf1eb196a3c034c7fc28e
MrEpoch <alexandrstencuk@seznam.cz>
2024-11-03 18:16:58 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -28,20 +28,49 @@ impl fmt::Display for TokenType {
 
 #[derive(Debug)]
 enum TokenType {
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
 
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
 
-    IDENTIFIER, STRING, NUMBER,
+    IDENTIFIER,
+    STRING,
+    NUMBER,
 
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
 
-    EOF
+    EOF,
 }
 #[derive(Debug)]
 struct Token {
@@ -57,17 +86,16 @@ impl Token {
             token_type,
             lexeme,
             literal,
-            line
+            line,
         }
-        }
+    }
 }
 
-    impl fmt::Display for Token {
+impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{} {} {:?}", self.token_type, self.lexeme, self.literal)
     }
 }
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -81,12 +109,10 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
-
 
             if !file_contents.is_empty() {
                 let mut error_code: u8 = 0;
@@ -99,7 +125,6 @@ fn main() {
                 if error_code == 65 {
                     exit(65);
                 }
-
             } else {
                 println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
             }
@@ -114,19 +139,140 @@ fn main() {
 fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut line = 1;
+    let mut current: usize = 0;
 
-    for c in source.chars() {
+    while current < source.len() {
+        let c = source.chars().nth(current).unwrap();
+        current += 1;
         match c {
-            '(' => tokens.push(Token::new(TokenType::LEFT_PAREN, String::from("("), Option::from(Literal::Nil), line)),
-            ')' => tokens.push(Token::new(TokenType::RIGHT_PAREN, String::from(")"), Option::from(Literal::Nil), line)),
-            '{' => tokens.push(Token::new(TokenType::LEFT_BRACE, String::from("{"), Option::from(Literal::Nil), line)),
-            '}' => tokens.push(Token::new(TokenType::RIGHT_BRACE, String::from("}"), Option::from(Literal::Nil), line)),
-            ',' => tokens.push(Token::new(TokenType::COMMA, String::from(","), Option::from(Literal::Nil), line)),
-            '.' => tokens.push(Token::new(TokenType::DOT, String::from("."), Option::from(Literal::Nil), line)),
-            '-' => tokens.push(Token::new(TokenType::MINUS, String::from("-"), Option::from(Literal::Nil), line)),
-            '+' => tokens.push(Token::new(TokenType::PLUS, String::from("+"), Option::from(Literal::Nil), line)),
-            ';' => tokens.push(Token::new(TokenType::SEMICOLON, String::from(";"), Option::from(Literal::Nil), line)),
-            '*' => tokens.push(Token::new(TokenType::STAR, String::from("*"), Option::from(Literal::Nil), line)),
+            '(' => tokens.push(Token::new(
+                TokenType::LEFT_PAREN,
+                String::from("("),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            ')' => tokens.push(Token::new(
+                TokenType::RIGHT_PAREN,
+                String::from(")"),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            '{' => tokens.push(Token::new(
+                TokenType::LEFT_BRACE,
+                String::from("{"),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            '}' => tokens.push(Token::new(
+                TokenType::RIGHT_BRACE,
+                String::from("}"),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            ',' => tokens.push(Token::new(
+                TokenType::COMMA,
+                String::from(","),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            '.' => tokens.push(Token::new(
+                TokenType::DOT,
+                String::from("."),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            '-' => tokens.push(Token::new(
+                TokenType::MINUS,
+                String::from("-"),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            '+' => tokens.push(Token::new(
+                TokenType::PLUS,
+                String::from("+"),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            ';' => tokens.push(Token::new(
+                TokenType::SEMICOLON,
+                String::from(";"),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            '*' => tokens.push(Token::new(
+                TokenType::STAR,
+                String::from("*"),
+                Option::from(Literal::Nil),
+                line,
+            )),
+            '!' => {
+                let is_bang = match_operator(source, &mut current);
+                tokens.push(Token::new(
+                    if is_bang {
+                        TokenType::BANG_EQUAL
+                    } else {
+                        TokenType::BANG
+                    },
+                    if is_bang {
+                        String::from("!=")
+                    } else {
+                        String::from("!")
+                    },
+                    Option::from(Literal::Nil),
+                    line,
+                ))
+            }
+            '=' => {
+                let is_equal = match_operator(source, &mut current);
+                tokens.push(Token::new(
+                    if is_equal {
+                        TokenType::EQUAL_EQUAL
+                    } else {
+                        TokenType::EQUAL
+                    },
+                    if is_equal {
+                        String::from("==")
+                    } else {
+                        String::from("=")
+                    },
+                    Option::from(Literal::Nil),
+                    line,
+                ))
+            }
+            '<' => {
+                let is_less = match_operator(source, &mut current);
+                tokens.push(Token::new(
+                    if is_less {
+                        TokenType::LESS_EQUAL
+                    } else {
+                        TokenType::LESS
+                    },
+                    if is_less {
+                        String::from("<=")
+                    } else {
+                        String::from("<")
+                    },
+                    Option::from(Literal::Nil),
+                    line,
+                ))
+            }
+            '>' => {
+                let is_greater = match_operator(source, &mut current);
+                tokens.push(Token::new(
+                    if is_greater {
+                        TokenType::GREATER_EQUAL
+                    } else {
+                        TokenType::GREATER
+                    },
+                    if is_greater {
+                        String::from(">=")
+                    } else {
+                        String::from(">")
+                    },
+                    Option::from(Literal::Nil),
+                    line,
+                ))
+            }
             '\n' => line += 1,
             _ => {
                 eprintln!("[line {line}] Error: Unexpected character: {c}");
@@ -135,6 +281,24 @@ fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
         }
     }
 
-    tokens.push(Token::new(TokenType::EOF, String::new(), Option::from(Literal::Nil), 0));
+    fn match_operator(source: &String, current: &mut usize) -> bool {
+        if source.len() <= *current {
+            return false;
+        }
+
+        if source.chars().nth(*current).unwrap() == '=' {
+            *current += 1;
+            return true;
+        }
+
+        false
+    }
+
+    tokens.push(Token::new(
+        TokenType::EOF,
+        String::new(),
+        Option::from(Literal::Nil),
+        0,
+    ));
     tokens
 }
