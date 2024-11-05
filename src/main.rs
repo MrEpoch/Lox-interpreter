04c057b615862bb776b017797d45784d95ac87b2
MrEpoch <alexandrstencuk@seznam.cz>
2024-11-05 21:58:59 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ enum Literal {
     Int(i32),
     Float(f32),
     Bool(bool),
-    Nil,
+    Null,
+}
+
+impl fmt::Display for Literal {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 impl fmt::Display for TokenType {
@@ -70,11 +76,11 @@ struct Token {
     token_type: TokenType,
     lexeme: String,
     literal: Option<Literal>,
-    line: i32,
+    line: u32,
 }
 
 impl Token {
-    fn new(token_type: TokenType, lexeme: String, literal: Option<Literal>, line: i32) -> Self {
+    fn new(token_type: TokenType, lexeme: String, literal: Option<Literal>, line: u32) -> Self {
         Self {
             token_type,
             lexeme,
@@ -112,7 +118,12 @@ fn main() {
                 let tokens: Vec<Token> = scan_tokens(&file_contents, &mut error_code);
 
                 for v in tokens.iter() {
-                    println!("{} {} null", v.token_type, v.lexeme);
+                    println!(
+                        "{} {} {}",
+                        v.token_type,
+                        v.lexeme,
+                        print_based_on_literal(&v.literal.as_ref().unwrap())
+                    );
                 }
 
                 if error_code == 65 {
@@ -129,75 +140,87 @@ fn main() {
     }
 }
 
+fn print_based_on_literal(literal: &Literal) -> String {
+    match literal {
+        Literal::String(s) => s.clone(),
+        Literal::Int(i) => String::from(i.to_string()),
+        Literal::Float(f) => String::from(f.to_string()),
+        Literal::Bool(b) => String::from(b.to_string()),
+        Literal::Null => String::from("null"),
+    }
+}
+
 fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
-    let mut line = 1;
+    let mut line: u32 = 1;
     let mut current: usize = 0;
+    let mut start: usize = 0;
     let mut char_array = source.chars().collect::<Vec<char>>();
     let char_count = char_array.len();
 
     while current < char_count {
         let c = char_array.get(current).unwrap();
+        start = current;
         current += 1;
         match c {
             '(' => tokens.push(Token::new(
                 TokenType::LEFT_PAREN,
                 String::from("("),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             ')' => tokens.push(Token::new(
                 TokenType::RIGHT_PAREN,
                 String::from(")"),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             '{' => tokens.push(Token::new(
                 TokenType::LEFT_BRACE,
                 String::from("{"),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             '}' => tokens.push(Token::new(
                 TokenType::RIGHT_BRACE,
                 String::from("}"),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             ',' => tokens.push(Token::new(
                 TokenType::COMMA,
                 String::from(","),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             '.' => tokens.push(Token::new(
                 TokenType::DOT,
                 String::from("."),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             '-' => tokens.push(Token::new(
                 TokenType::MINUS,
                 String::from("-"),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             '+' => tokens.push(Token::new(
                 TokenType::PLUS,
                 String::from("+"),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             ';' => tokens.push(Token::new(
                 TokenType::SEMICOLON,
                 String::from(";"),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             '*' => tokens.push(Token::new(
                 TokenType::STAR,
                 String::from("*"),
-                Option::from(Literal::Nil),
+                Option::from(Literal::Null),
                 line,
             )),
             '!' => {
@@ -213,7 +236,7 @@ fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
                     } else {
                         String::from("!")
                     },
-                    Option::from(Literal::Nil),
+                    Option::from(Literal::Null),
                     line,
                 ))
             }
@@ -230,7 +253,7 @@ fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
                     } else {
                         String::from("=")
                     },
-                    Option::from(Literal::Nil),
+                    Option::from(Literal::Null),
                     line,
                 ))
             }
@@ -247,7 +270,7 @@ fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
                     } else {
                         String::from("<")
                     },
-                    Option::from(Literal::Nil),
+                    Option::from(Literal::Null),
                     line,
                 ))
             }
@@ -264,15 +287,15 @@ fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
                     } else {
                         String::from(">")
                     },
-                    Option::from(Literal::Nil),
+                    Option::from(Literal::Null),
                     line,
                 ))
             }
             '/' => {
                 let matched = match_operator(&mut char_array, &mut current, '/', char_count);
                 if matched {
-                    while peek(&mut char_array, &mut current, char_count) != '\n'
-                        && !is_end(&mut current, char_count)
+                    while peek(&mut char_array, current, char_count) != '\n'
+                        && !is_end(current, char_count)
                     {
                         current += 1;
                     }
@@ -280,9 +303,30 @@ fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
                     tokens.push(Token::new(
                         TokenType::SLASH,
                         String::from("/"),
-                        Option::from(Literal::Nil),
+                        Option::from(Literal::Null),
                         line,
                     ));
+                }
+            }
+            '"' => {
+                match string_process(&mut char_array, &mut current, char_count, start, &mut line) {
+                    Ok(string) => {
+                        *error_code = 0;
+                        tokens.push(Token::new(
+                            TokenType::STRING,
+                            string.clone(),
+                            Option::from(if string.len() > 3 {
+                                // Need to cut \ for string value "
+                                Literal::String(string[1..string.len() - 1].to_string())
+                            } else {
+                                Literal::String(String::new())
+                            }),
+                            line,
+                        ));
+                    }
+                    Err(_) => {
+                        *error_code = 65;
+                    }
                 }
             }
             ' ' | '\r' | '\t' => (),
@@ -294,8 +338,38 @@ fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
         }
     }
 
-    fn is_end(current: &mut usize, char_count: usize) -> bool {
-        if char_count <= *current {
+    fn string_process(
+        char_array: &mut Vec<char>,
+        current: &mut usize,
+        char_count: usize,
+        start: usize,
+        line: &mut u32,
+    ) -> Result<String, u8> {
+        let mut peeked_value: char = peek(char_array, *current, char_count);
+        while peeked_value != '"' && !is_end(*current, char_count) {
+            if peeked_value == '\n' {
+                *line += 1;
+            }
+            *current += 1;
+            peeked_value = peek(char_array, *current, char_count);
+        }
+
+        if is_end(*current, char_count) {
+            eprintln!("[line {line}] Error: Unterminated string.");
+            return Err(65);
+        }
+
+        *current += 1;
+
+        if (*current - 2) == start {
+            Ok(['"', '"'].iter().collect::<String>())
+        } else {
+            Ok(char_array[start..*current].iter().collect::<String>())
+        }
+    }
+
+    fn is_end(current: usize, char_count: usize) -> bool {
+        if char_count <= current {
             true
         } else {
             false
@@ -308,26 +382,26 @@ fn scan_tokens(source: &String, error_code: &mut u8) -> Vec<Token> {
         operator: char,
         char_count: usize,
     ) -> bool {
-        if is_end(current, char_count) || (*char_array.get(*current).unwrap() != operator) {
+        if is_end(*current, char_count) || (*char_array.get(*current).unwrap() != operator) {
             return false;
         }
         *current += 1;
         true
     }
 
-    fn peek(char_array: &mut Vec<char>, current: &mut usize, char_count: usize) -> char {
+    fn peek(char_array: &mut Vec<char>, current: usize, char_count: usize) -> char {
         if is_end(current, char_count) {
             '\0'
         } else {
-            *char_array.get(*current).unwrap()
+            *char_array.get(current).unwrap()
         }
     }
 
     tokens.push(Token::new(
         TokenType::EOF,
         String::new(),
-        Option::from(Literal::Nil),
-        0,
+        Option::from(Literal::Null),
+        line,
     ));
     tokens
 }
