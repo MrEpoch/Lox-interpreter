8d0fe544fd5c5b3c29a272435b667c9f54d5c3dc
MrEpoch <alexandrstencuk@seznam.cz>
2024-11-14 17:18:47 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -226,7 +226,7 @@ fn main() {
                         println!("{}", print_based_on_literal(&l));
                     }
                     Expr::Unary { operator, right } => {
-                        println!("({})", get_from_unary(Expr::Unary { operator, right }));
+                        println!("{}", get_from_unary(Expr::Unary { operator, right }));
                     }
                     Expr::String(s) => {
                         println!("{}", s);
@@ -288,7 +288,7 @@ fn handle_match(expr: Expr, left_side: &String, right_side: &String) -> String {
                 format!("{left_side}{}{right_side}", print_based_on_literal(&l))
             }
             Expr::Unary { operator, right } => {
-                format!("{left_side}({}){right_side}", get_from_unary(Expr::Unary { operator, right }))
+                format!("{left_side}({} {}){right_side}", operator.lexeme, handle_match(*right, &String::from(""), &String::from("")))
             }
             Expr::String(s) => {
                 format!("{left_side}{s}{right_side}")
@@ -304,7 +304,7 @@ fn handle_match(expr: Expr, left_side: &String, right_side: &String) -> String {
 
 fn get_from_unary(expr_unary: Expr) -> String {
     if let Expr::Unary { operator, right } = expr_unary {
-        format!("{} {}", operator.lexeme, get_from_unary(*right))
+        format!("({} {})", operator.lexeme, get_from_unary(*right))
     } else if let Expr::Literal(literal) = expr_unary {
         print_based_on_literal(&literal)
     }
