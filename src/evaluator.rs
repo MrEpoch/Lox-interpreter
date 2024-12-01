6ab8ae39edc4a7e8a0ae35e959de082848872ce8
MrEpoch <alexandrstencuk@seznam.cz>
2024-12-01 22:06:40 +0100

    codecrafters submit [skip ci]

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -13,6 +13,8 @@ impl Evaluator {
     }
 
     pub fn evaluate(&self) -> Expr {
+        self.evaluator(&self.expression)
+            /*
         match &self.expression {
             Expr::Literal(l) => {
                 match l {
@@ -24,6 +26,32 @@ impl Evaluator {
                     _ => Expr::Nil,
                 }
             }
+            Expr::Grouping(exprs) => {
+                for e in exprs {
+                    self.evaluate();
+                }
+                Expr::Nil
+            },
+            _ => Expr::Nil,
+        }
+        */
+    }
+
+    fn evaluator(&self, expr: &Expr) -> Expr {
+        match expr {
+            Expr::Literal(l) => {
+                match l {
+                    Literal::Bool(b) => Expr::Bool(*b),
+                    Literal::String(s) => {
+                        Expr::String(s.clone())
+                    },
+                    Literal::Number(n) => Expr::Number(n.0),
+                    _ => Expr::Nil,
+                }
+            }
+            Expr::Grouping(exprs) => {
+                self.evaluator(&exprs[0])
+            },
             _ => Expr::Nil,
         }
     }
